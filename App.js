21ecc3a4b07118a8682bfc890e1a158d3192dd96
tornike jandieri <tornikejandieri7@gmail.com>
2022-11-17 01:11:39 +0400

    input and list

--- App.js
+++ App.js
@@ -9,7 +9,9 @@ import {
   ScrollView,
   FlatList,
   TouchableOpacity,
+  Alert,
 } from "react-native"
+import AddTodo from "./components/AddTodo"
 import Header from "./components/Header"
 import TodoItem from "./components/TodoItem"
 
@@ -19,16 +21,30 @@ export default function App() {
     { text: "create an app", key: "2" },
     { text: "play on the switch", key: "3" },
   ])
+
   const pressHandler = (key) => {
     setTodos((prevTodos) => {
       return prevTodos.filter((todo) => todo.key !== key)
     })
   }
+
+  const submitHandler = (text) => {
+    if (text.length > 3) {
+      setTodos((prevTodos) => {
+        return [{ text: text, key: Math.random().toString() }, ...prevTodos]
+      })
+    } else {
+      Alert.alert("OOPS!", "Todos must be over 3 chars long", [
+        { text: "Understood", onPress: () => console.log("alert closed") },
+      ])
+    }
+  }
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* todo form */}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
