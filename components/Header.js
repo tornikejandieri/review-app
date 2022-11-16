fd28ef44866e5183b4d15a2a292139cf1073ba1b
tornike jandieri <tornikejandieri7@gmail.com>
2022-11-17 00:46:49 +0400

    header

---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native"
+
+const Header = () => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>My todos</Text>
+    </View>
+  )
+}
+
+export default Header
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    paddingTop: 38,
+    backgroundColor: "coral",
+  },
+  title: {
+    textAlign: "center",
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+})
